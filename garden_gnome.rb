78109454befc567430ad5cfc3e80e9cb119b87b4
Hunter Sapienza <sapienzahunter@gmail.com>
2018-07-28 16:35:32 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,7 +1,8 @@
 # Code your instances here
 class GardenGnome
 
-attr_accessor :name, :age, :gluten_allergy, :personality, :hat_color
+attr_reader :hat_color
+attr_accessor :name, :age, :gluten_allergy, :personality
 
 def initialize(hat_color, personality = "evil")
   @personality = personality
